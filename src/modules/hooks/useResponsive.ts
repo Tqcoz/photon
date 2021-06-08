06e3968bdcfcfc4626a9c295edf5308fc695e5e5
Tqcoz <eclectictreeminecraftgaming@gmail.com>
2021-06-08 09:56:42 +0000

    Revert TailwindJIT and Media Queries

--- src/modules/hooks/useResponsive.ts
+++ src/modules/hooks/useResponsive.ts
@@ -1,9 +1,9 @@
 import { useMediaQuery } from "react-responsive";
 
 export const useResponsive = ():"3-cols" | "2-cols" | "1-cols" | "fullscreen" => {
-  const is3Cols = useMediaQuery({ minDeviceWidth: 1336 });
-  const is2Cols = useMediaQuery({ minDeviceWidth: 1265 });
-  const is1Cols = useMediaQuery({ minDeviceWidth: 800 });
+  const is3Cols = useMediaQuery({ minWidth: 1336 });
+  const is2Cols = useMediaQuery({ minWidth: 1265 });
+  const is1Cols = useMediaQuery({ minWidth: 800 });
 
   if (is3Cols) {
     return "3-cols";
