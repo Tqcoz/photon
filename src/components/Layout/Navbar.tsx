243ee0bf43444ab4e07a677fb069bd71e1844396
Tqcoz <eclectictreeminecraftgaming@gmail.com>
2021-06-09 23:01:45 +0000

    revamp navbar

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -1,76 +1,74 @@
-import { AppBar, Button, IconButton, Toolbar } from '@material-ui/core';
+import {
+  AppBar,
+  Button,
+  IconButton,
+  Toolbar
+} from '@material-ui/core';
 import Link from 'next/link';
-import React, { SetStateAction } from 'react';
+import React, {
+  SetStateAction
+} from 'react';
 import MenuButton from '@material-ui/icons/Menu'
-import { useResponsive } from '../../modules/hooks/useResponsive';
-import { useNavDrawer } from '../../modules/contexts/Drawer';
+import {
+  useResponsive
+} from '../../modules/hooks/useResponsive';
+import {
+  useNavDrawer
+} from '../../modules/contexts/Drawer';
 import NavigationDrawer from './NavigationDrawer';
-import { useAuthenticated } from '../../modules/contexts/Authentication';
-import { useMediaQuery } from '@chakra-ui/media-query';
+import {
+  useAuthenticated
+} from '../../modules/contexts/Authentication';
+import {
+  useMediaQuery
+} from '@chakra-ui/media-query';
 const Navbar = () => {
   let query = useResponsive()
-  let { open } = useNavDrawer()
+  let {
+      open
+  } = useNavDrawer()
 
   const [isSmallScreen] = useMediaQuery(["(max-width:700px)"])
-  const {authenticated} = useAuthenticated()
+  const {
+      authenticated
+  } = useAuthenticated()
   console.log(query);
-  
+
   return (
     <AppBar style={{background: '#f2636d'}} color="primary" className="_nav" classes={{colorPrimary:'flex-1 bg-brand bg-opacity-25', root: 'bg-opacity-25 bg-brand flex flex-row flex-wrap justify-start flex-1 px-4 py-2 m-auto text-gray-100 shadow-sm flex-0'}} position="static" elevation={1}>
-      <Toolbar className="bg-brand">
-        <h1 className="items-center p-4 text-2xl font-semibold text-center md:items-start md:text-left md:p-auto">Photon</h1>
-        <div className="w-full flex flex-row justify-self-end">
-          {
-            isSmallScreen ? (
-              <>
-                {
-                  authenticated ? (
-                    <Button variant="outlined" color="secondary">
-                      Open Photon
-                    </Button>
-                  ) : (
-                    <Button href="/login" variant="outlined" color="secondary">
-                      Login
-                    </Button>
-                  )
-                }
-                <IconButton onClick={open} classes={{root: "flex md:hidden"}}>
-                  <MenuButton />
-                </IconButton>
-                <NavigationDrawer />
-              </>
-            ) : (
-              <>
-                <div className="flex flex-row justify-self-end items-center self-center justify-center flex-1 gap-4 text-base font-medium text-center text-gray-200">
-                  <Link href="/privacy">
-                    <span className="self-center text-center hover:underline hover:cursor-pointer">
-                      Privacy
-                    </span>
-                  </Link>
-                  <Link href="/faq">
-                    <span className="self-center text-center hover:underline hover:cursor-pointer">
-                      FAQ
-                    </span>
-                  </Link>
-                  <Link href="/downloads">
-                    <span className="self-center text-center hover:underline hover:cursor-pointer">
-                      Downloads
-                    </span>
-                  </Link>
+      <Toolbar className="bg-brand flex flex-row">
+        <div className="w-full justify-self-start flex-0">
+          <h1 className="items-center p-4 text-2xl font-semibold flex-1 text-center md:items-start md:text-left md:p-auto">Photon</h1>
+        </div>
+        <div className="w-full flex flex-row justify-self-end self-center flex-1">
+    
+          {/* {/* <div className="flex flex-row justify-self-end items-center self-center justify-center flex-1 gap-4 text-base font-medium text-center text-gray-200"> */}
+            {/* <Link href="/privacy">
+              <span className="self-center text-center hover:underline hover:cursor-pointer">
+                Privacy
+              </span>
+            </Link>
+            <Link href="/faq">
+              <span className="self-center text-center hover:underline hover:cursor-pointer">
+                FAQ
+              </span>
+            </Link> */}
+            {/* <Link href="/downloads">
+              <span className="self-center text-center hover:underline hover:cursor-pointer">
+                Downloads
+              </span>
+            </Link> */} 
 
-                </div>
-                {
-                  authenticated ? (
-                    <Button variant="outlined" color="secondary">
-                      Open Photon
-                    </Button>
-                  ) : (
-                    <Button href="/login" variant="outlined" color="secondary">
-                      Login
-                    </Button>
-                  )
-                }
-              </>
+          {/* </div> */}
+          {
+            authenticated ? (
+              <Button variant="outlined" color="secondary">
+                Open Photon
+              </Button>
+            ) : (
+              <Button href="/login" variant="outlined" color="secondary">
+                Login
+              </Button>
             )
           }
         </div>
--- src/components/Layout/SEO/basic.tsx
+++ src/components/Layout/SEO/basic.tsx
@@ -2,17 +2,20 @@ import React from 'react';
 import Head from 'next/head';
 
 const Basic = () => {
+  const descripton = "Photon is a revolutionary messaging app designed to fit users needs best while still remaining easy to use and customizable, it uses top-tier security protocols to keep you messaging anonymously."
   return (
     <>
       <meta name="theme-color" content={'#f2636d'} />
-      <meta name="description" content={'Photon is a revolutionary chat client designed to fit users needs best while still remaining easy to use and customizable, it uses top-tier security protocols to keep you messaging anonymously.'} />
-      <meta name="keywords" content="messaging,communications,anonymous messaging,fast messaging,photon,getphoton,messaging app,free messaging app,free messaging" />
+      <meta name="description" content={descripton} />
+      <meta name="keywords" content="messaging,messaging app,client,user messaging,encrypted messaging,photon app,getphotonapp,get photon app,photon messaging,message program,communications,anonymous messaging,fast messaging,photon,getphoton,messaging app,free messaging app,free messaging" />
       <meta property="og:type" content="website" />
-      <meta property="og:description" content={'Photon is a revolutionary chat client designed to fit users needs best while still remaining easy to use and customizable, it uses top-tier security protocols to keep you messaging anonymously.'} />
-      <meta property="twitter:description" content={'Photon is a revolutionary chat client designed to fit users needs best while still remaining easy to use and customizable, it uses top-tier security protocols to keep you messaging anonymously.'} />
+      <meta property="og:description" content={descripton} />
+      <meta property="twitter:description" content={descripton} />
       <meta property="og:site_name" content={'Photon'} />
       <meta property="twitter:card" content="summary" />
       <meta property="twitter:title" content={'Photon'} />
+      <meta name={"robots"} content={"index, follow"} />
+      <meta property="og:image" content="https://server.getphoton.app/files/logo.png" />
 
     </>
   );
